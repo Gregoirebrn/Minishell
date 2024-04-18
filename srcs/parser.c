c00baae2e4686079beb2cda8126717d7871d7441
Sweekt <benjamin.roy69@gmail.com>
2024-04-18 12:13:39 +0200

    Pre-parsing check working fine (returning error in case of unterminated quote or double pipes)

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,14 +6,15 @@
 /*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:19:51 by beroy             #+#    #+#             */
-/*   Updated: 2024/04/17 14:36:14 by beroy            ###   ########.fr       */
+/*   Updated: 2024/04/17 19:07:39 by beroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int quote_skip(char *input, int *i, char find)
+void quote_skip(char *input, int *i, char find)
 {
+	*i += 1;
 	while (input[*i] != 0 && input[*i] != find)
 		*i += 1;
 	if (input[*i] == 0)
@@ -23,29 +24,22 @@ int quote_skip(char *input, int *i, char find)
 int check_line(char *input)
 {
 	int i;
-	int qc;
+
 
 	i = 0;
-	qc = 0;
 	while (input[i])
 	{
 		if (input[i] == '|')
+		{
 			if (input[i + 1] == '|')
-				return (printf("double pipe not handled by minishell!"), 1);
-		if (input[i] == 34)
-		{
-			qc++;
+				return (printf("double pipe not handled by minishell!\n"), 1);
+		}
+		else if (input[i] == 34)
 			quote_skip(input, &i, 34);
-		}
-		if (input[i] == 39)
-		{
-			qc++;
+		else if (input[i] == 39)
 			quote_skip(input, &i, 39);
-		}
 		i++;
 	}
-	if (qc % 2 != 0)
-		return (printf("quote unterminated!"), 1);
 	return (0);
 }
 
@@ -53,4 +47,5 @@ t_head	*ft_parse(char *input)
 {
 	if (check_line(input) == 1)
 		return (NULL);
+	return (NULL);
 }
