3f68e2666f8cc246fa6f0a900cd2392451523820
Grgoirebrn <gregoire2246@gmail.com>
2024-04-18 16:41:17 +0200

    create builtins.h

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 21:41:07 by grebrune          #+#    #+#             */
-/*   Updated: 2024/04/17 21:41:07 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/04/18 15:22:40 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 #define MINISHELL_BUILTINS_H
 
 #include "errno.h"
+#include "minishell.h"
 
+typedef struct s_env t_env;
+typedef struct s_redir t_redir;
+typedef struct s_cmd t_cmd;
+typedef struct s_head t_head;
+
+char	*ft_strcat(char *path, char *dir);
+int		get_path(char **str);
+
+void	ft_echo(t_head *head);
+void	ft_pwd(void);
+int		ft_cd(t_head *head);
+void	ft_export(t_head *head);
 
 #endif
