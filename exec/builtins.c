bab0b00d464164feed58d4968efa413b395c1093
Grgoirebrn <gregoire2246@gmail.com>
2024-04-22 16:31:25 +0200

    change of dir test of export done partially

---
+++ exec/builtins.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 13:30:25 by grebrune          #+#    #+#             */
+/*   Updated: 2024/04/18 18:26:22 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+//echo a tester
+
+void	ft_echo(t_head *head)
+{
+	t_cmd	*copy;
+	int		i;
+
+	i = 0;
+	copy = head->cmd;
+	while (copy->next != NULL)
+	{
+		if (i == 1 &&  0 == ft_strncmp(copy->arg[i], "-n", ft_strlen(copy->arg[i])))
+			i = -1;
+		printf("%s ", copy->arg[i]);
+		copy = copy->next;
+		if (i > -1)
+			i++;
+	}
+	if (0 == ft_strncmp(copy->arg[i], "-n", ft_strlen(copy->arg[i])))
+		printf("%s", copy->arg[i]);
+	else
+		printf("%s\n", copy->arg[i]);
+}
+
+void	ft_pwd(void)
+{
+	int		err;
+	char	*str;
+
+	str = NULL;
+	err = get_path(&str);
+	if (err == 2)
+	{
+		write(1, "bash: pwd: ", 10);
+		perror(str);
+	}
+	printf("%s\n", str);
+	free(str);
+}
+
+int	ft_cd(t_head *head)
+
+{
+	int		err;
+	char	*str;
+	char	*old_pwd;
+
+	old_pwd = NULL;
+	str = NULL;
+	get_path(&str);
+	str = ft_strcat(str, head->cmd->arg[1]);
+	if (str == NULL)
+		return (printf("Crash of Malloc\n"), 2);
+	get_path(&old_pwd);
+	err = chdir(str);
+	free(str);
+	if (err != 0)
+		return (write(1, "bash: cd: ", 10), perror(head->cmd->arg[1]), 2);
+	change_old_pwd(head, old_pwd);
+	return (0);
+}
+
+void	ft_export(t_head *head)
+{
+	t_head	*copy;
+
+	copy = head;
+	while (copy->env->name)
+	{
+		if (0 == strncmp(copy->env->name, copy->cmd->arg[0],
+						 strlen(copy->cmd->arg[0])))
+		{
+			replace_var(&copy->env->value, copy->cmd->arg[1]);
+			return ;
+		}
+		copy->env = copy->env->next;
+	}
+}
+
+void	ft_unset(t_head *head)
+{
+	rem_env(&head->env, head->cmd->arg[1], &ft_strncmp);
+}
---
+++ exec/builtins_utils2.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins_utils2.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 18:04:21 by grebrune          #+#    #+#             */
+/*   Updated: 2024/04/18 18:28:36 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	change_old_pwd(t_head *head, char *old_pwd)
+{
+	t_env	*copy;
+
+	copy = head->env;
+	while (copy->next != NULL)
+	{
+		if (ft_strncmp(copy->name, "OLDPWD", 7))
+		{
+			free(copy->value);
+			copy->value = old_pwd;
+		}
+		copy = copy->next;
+	}
+}
+
+void	replace_var(char **arg, char *result)
+{
+	int		z;
+
+	free(*(arg));
+	*arg = calloc((strlen(result) + 1), sizeof (char));
+	z = 0;
+	while (result[z])
+	{
+		*(arg[z]) = result[z];
+		z++;
+	}
+	*(arg[z]) = '\0';
+}
