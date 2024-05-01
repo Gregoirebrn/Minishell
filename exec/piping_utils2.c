6c74724e4578d86ca0ef25ece1048f93991289b3
Grgoirebrn <gregoire2246@gmail.com>
2024-05-01 16:18:30 +0200

    make_arg done

--- exec/piping_utils2.c
+++ exec/piping_utils2.c
@@ -6,7 +6,7 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 18:51:28 by grebrune          #+#    #+#             */
-/*   Updated: 2024/04/30 18:51:28 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/05/01 16:09:54 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,61 @@ void	wait_for_all(pid_t *pid, int x)
 	while (i < x)
 	{
 		waitpid(pid[i], NULL, 0);
-		i++:
+		i++;
 	}
+}
+
+size_t	envlen(t_env *base)
+{
+	size_t	size;
+	t_env	*copy;
+
+	copy = base;
+	size = 0;
+	if (copy != NULL)
+		size++;
+	while (copy->next != NULL)
+	{
+		size++;
+		copy = copy->next;
+	}
+	return (size);
+}
+
+size_t	cmdlen(t_cmd *base)
+{
+	size_t	size;
+	t_cmd	*copy;
+
+	copy = base;
+	size = 0;
+	if (copy != NULL)
+		size++;
+	while (copy->next != NULL)
+	{
+		size++;
+		copy = copy->next;
+	}
+	return (size);
+}
+
+char	**make_arg(t_cmd *cmd)
+{
+	char	**arg;
+	int		x;
+
+	x = 0;
+	while (cmd->arg && cmd->arg[x])
+		x++;
+	arg = malloc(sizeof (char *) * (x + 1));
+	if (arg == NULL)
+		return (NULL);
+	x = 0;
+	while (cmd->arg && cmd->arg[x])
+	{
+		arg[x] = ft_strdup(cmd->arg[x]);
+		x++;
+	}
+	arg[x] = NULL;
+	return (arg);
 }
