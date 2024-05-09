06f52acf2423fbb04c76ecfc2c25d6a77dbc355a
grebrune <grebrune@student.42lyon.fr>
2024-05-09 14:49:27 +0200

    create to add dup_of_fd

---
+++ exec/piping_utils3.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   piping_utils3.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 14:44:44 by grebrune          #+#    #+#             */
+/*   Updated: 2024/05/09 14:44:44 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/builtins.h"
+
+void	dup_of_fd(int fd[2], t_cmd *copy)
+{
+	if (copy->next != NULL)
+		if (0 > dup2(fd[1], 1))
+			return (perror("dup2"));
+	if (copy->prev != NULL)
+		if (0 > dup2(fd[0], 0))
+			return (perror("dup2"));
+}
