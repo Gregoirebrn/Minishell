7ec019e57e737187b7f275ab97d80fb853391f16
beroy <benjamin.roy69@gmail.com>
2024-05-28 15:52:22 +0200

    merge of branch corrected

--- srcs/format.c
+++ srcs/format.c
@@ -6,7 +6,7 @@
 /*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 15:00:48 by beroy             #+#    #+#             */
-/*   Updated: 2024/04/25 16:11:49 by beroy            ###   ########.fr       */
+/*   Updated: 2024/05/28 15:30:18 by beroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char *trim_str(char *str)
 	i = 0;
 	if (str[i] != 34 && str[i] != 39)
 		return (str);
-	len = ft_strlen(str);
+	len = (int)ft_strlen(str);
 	trim = calloc(len - 2 + 1, sizeof(char));
 	if (trim == NULL)
 		return (NULL);
@@ -55,6 +55,55 @@ int format(t_head *head)
 	{
 		if (trim_tab(head->cmd->arg) == 1)
 			return (1);
+		if (head->cmd->next == NULL)
+			break ;
+		head->cmd = head->cmd->next;
+	}
+	while (head->cmd->prev)
+		head->cmd = head->cmd->prev;
+	return (0);
+}
+
+int	format_redir(t_cmd *cmd)
+{
+	while (cmd)
+	{
+		while (find_redir(cmd->line) == 1)
+		{
+			if (extract_redir(cmd) == 1)
+				return (1);
+		}
+		if (cmd->next == NULL)
+			break ;
+		cmd = cmd->next;
+	}
+	while (cmd->prev)
+		cmd = cmd->prev;
+	return (0);
+}
+
+int format_var(t_head *head)
+{
+	while (head->cmd)
+	{
+		while (find_var(head->cmd->line) != -1)
+		{
+			if (replace_var_line(head->cmd->line, head) == 1)
+				return (1);
+		}
+		while (head->cmd->redir)
+		{
+			while (find_var(head->cmd->redir->arg) != -1)
+			{
+				if (replace_var_redir(head->cmd->redir->arg, head) == 1)
+					return (1);
+			}
+			if (head->cmd->redir->next == NULL)
+				break ;
+			head->cmd->redir = head->cmd->redir->next;
+		}
+		while (head->cmd->redir && head->cmd->redir->prev)
+			head->cmd->redir = head->cmd->redir->prev;
 		if (head->cmd->next == NULL)
 			break ;
 		head->cmd = head->cmd->next;
---
+++ srcs/format_utils_2.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   format_utils_2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 13:55:23 by beroy             #+#    #+#             */
+/*   Updated: 2024/05/28 15:38:08 by beroy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int find_var(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+			return (i);
+		single_index_up(str, &i);
+	}
+	return (-1);
+}
+
+int str_cmp_var(char *str, int i, char *value)
+{
+	int j;
+
+	j = 0;
+	while (str[i] && value[j] && str[i] == value[j])
+	{
+		i++;
+		j++;
+	}
+	if ((str[i] == 0 || str[i] == '$' || str[i] == 34 || char_is_ws(str[i])) && value[j] == 0)
+		return (1);
+	return (0);
+}
+
+char *var_value(char *str, int i, t_env *env)
+{
+	char	*value;
+
+	value = NULL;
+	while (env && value == NULL)
+	{
+		if (str_cmp_var(str, i, env->name) == 1)
+		{
+			value = ft_strdup(env->value);
+			if (value == NULL)
+			{
+				while (env->prev)
+					env = env->prev;
+				return (NULL);
+			}
+		}
+		if (env->next == NULL)
+			break ;
+		env = env->next;
+	}
+	if (value == NULL && str_cmp_var(str, i, "?"))
+		value = ft_strdup("'$?'");
+	while (env->prev)
+		env = env->prev;
+	return (value);
+}
+
+int find_end_var(char *str, int i)
+{
+	i++;
+	while (str[i])
+	{
+		if (char_is_ws(str[i]) == 1 || str[i] == '$' || str[i] == 34)
+			break ;
+		single_index_up(str, &i);
+	}
+	return (i);
+}
+
+int replace_var_line(char *str, t_head *head)
+{
+	int		start;
+	int 	end;
+	char	*value;
+
+	start = find_var(str);
+	end = find_end_var(str, start);
+	value = var_value(str, start + 1, head->env);
+	if (value == NULL)
+		return (1);
+	head->cmd->line = str_dup_var(str, start, end, value);
+	if (str == NULL)
+		return (1);
+	return (0);
+}
