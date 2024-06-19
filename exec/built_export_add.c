9cff946a1f1b236778292f6cd3679d23e7afe29b
grebrune <grebrune@student.42lyon.fr>
2024-06-19 20:12:48 +0200

    to do

---
+++ exec/built_export_add.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   built_export_add.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 20:11:01 by grebrune          #+#    #+#             */
+/*   Updated: 2024/06/19 20:11:01 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+//to do
+//addition in export
+// separate search of name in the export main function
