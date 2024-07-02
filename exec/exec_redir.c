4e428cb08a8469931ab93f112201226503b1a4f7
Grgoirebrn <gregoire2246@gmail.com>
2024-07-02 22:42:32 +0200

    parse all redir

--- exec/exec_redir.c
+++ exec/exec_redir.c
@@ -6,7 +6,7 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 14:44:44 by grebrune          #+#    #+#             */
-/*   Updated: 2024/07/02 14:36:20 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/07/02 22:38:47 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int	open_the_pipe(int **pipe, t_head *head)
 	return (0);
 }
 
-int	open_files(t_redir *redir)
+int	open_files(t_redir *redir, int err_print)
 {
 	if (redir->type == 1)
 		redir->fd = open(redir->arg, O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -52,26 +52,39 @@ int	open_files(t_redir *redir)
 	if ((redir->type == 4 && redir->fd == -1) || redir->type == 3)
 		redir->fd = open(redir->arg, O_RDONLY);
 	if (redir->fd == -1)
-		return (perror(redir->arg), 2);
+	{
+		if (err_print == 0)
+			perror(redir->arg);
+		return (2);
+	}
 	return (0);
 }
 
 int	open_redir(t_cmd *copy, int fd[2])
 {
 	t_redir	*parser;
+	t_cmd	*c_cmd;
+	int		err_print;
 
 	fd[1] = 1;
 	fd[0] = 0;
-	parser = copy->redir;
-	while (parser)
+	c_cmd = copy;
+	err_print = 0;
+	while (c_cmd)
 	{
-		if (open_files(parser))
-			return (2);
-		if (parser->type == 1 || parser->type == 2)
-			fd[1] = parser->fd;
-		if (parser->type == 3 || parser->type == 4)
-			fd[0] = parser->fd;
-		parser = parser->next;
+		parser = c_cmd->redir;
+		while (parser)
+		{
+			if (open_files(parser, err_print))
+				return (2);
+			if (parser->type == 1 || parser->type == 2)
+				fd[1] = parser->fd;
+			if (parser->type == 3 || parser->type == 4)
+				fd[0] = parser->fd;
+			parser = parser->next;
+		}
+		c_cmd = c_cmd->next;
+		err_print++;
 	}
 	return (0);
 }
