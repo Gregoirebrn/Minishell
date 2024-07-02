0b351e6d1cdba5b3d0c55279fe8239ff574908ca
Grgoirebrn <gregoire2246@gmail.com>
2024-07-02 19:51:30 +0200

    add exec_error file

---
+++ exec/exec_error.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_error.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/02 19:44:43 by grebrune          #+#    #+#             */
+/*   Updated: 2024/07/02 19:46:59 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	exec_error_exit(char **arg, char *str, char **env, char **path)
+{
+	perror(arg[0]);
+	free_tab(env);
+	free_tab(path);
+	free_tab(arg);
+	ft_free(str);
+	exit (127);
+}
